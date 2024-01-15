52566087933a6c9bef7a57079575cc47ffa2e74b
Omar Badr <omarbadr938@gmail.com>
2024-01-15 12:46:47 +0100

    x

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { Row, Col, Button } from 'react-bootstrap';
+import { FaTrash } from 'react-icons/fa';
+import { UseSelector, useDispatch, useSelector } from 'react-redux';
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const cart = useSelector((state) => state.cart.content);
+  const dispatch = useDispatch();
+  return (
+    <Row>
+      <Col sm={12}>
+        <ul style={{ listStyle: 'none' }}>
+          {cart.map((book, i) => {
+            return (
+              <li key={i} className='my-4'>
+                <Button
+                  variant='danger'
+                  onClick={() =>
+                    dispatch({ type: 'REMOVE_FROM_CART', payload: i })
+                  }
+                >
+                  <FaTrash />
+                </Button>
+                <img src={book.imageUrl} alt='Libro selezionato' />
+                {book.title}
+              </li>
+            );
+          })}
+        </ul>
+      </Col>
+      <Row>
+        <Col sm={12}>
+          TOTALE:{' '}
+          {cart.reduce(
+            (acc, currentValue) => acc + parseFloat(currentValue.price),
+            0
+          )}{' '}
+          €
+        </Col>
+      </Row>
+    </Row>
+  );
 };
 
 export default Cart;
--- src/components/CartIndicator.jsx
+++ src/components/CartIndicator.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';
+import { UseSelector, useSelector } from 'react-redux';
+import { Row, Button } from 'react-bootstrap';
+
+// useSelector restituisce un valore a partire da un metodo di callback, in cui ricavo come parametro il redux store
 
 const CartIndicator = () => {
-  return <div>CartIndicator</div>;
+  const navigate = useNavigate();
+  const cartLength = useSelector((state) => state.cart.content.length);
+  return (
+    <div className='d-flex justify-content-end my-4'>
+      <Button onClick={() => navigate('/cart')} className='align-items-center'>
+        <FaShoppingCart />
+        <span className='ms-2'>{cartLength}</span>
+      </Button>
+    </div>
+  );
 };
 
 export default CartIndicator;
